f133732da7860eebe2a86cc203c2bad4da3054a2
soccermansyu <118705210+soccermansyu@users.noreply.github.com>
2023-04-02 20:07:16 +0900

    Update APP.py

--- APP.py
+++ APP.py
@@ -14,18 +14,21 @@ def main():
     st.write('練習頻度')
     freq = st.selectbox('freq', ['3回/週', '4回/週', '5回/週', '6回/週', '7回/週'])
 
-    # 追加: Easy Pace, Threshold Pace, Interval Pace の入力フォームを作成
+    # 追加: Easy Pace, Moderate Pace, Threshold Pace, Interval Pace の入力フォームを作成
     st.write('Easy Pace (/km) (m:ss)')
-    easy_pace = st.text_input('easy_pace', value='0:00')
+    easy_pace = st.text_input('Easy_pace', value='0:00')
+    
+    st.write('Moderate Pace (/km) (m:ss)')
+    moderate_pace = st.text_input('Moderate_pace', value='0:00')    
     
     st.write('Threshold Pace (/km) (m:ss)')
-    threshold_pace = st.text_input('threshold_pace', value='0:00')
+    threshold_pace = st.text_input('Threshold_pace', value='0:00')
     
     st.write('Interval Pace (/km) (m:ss)')
-    interval_pace = st.text_input('interval_pace', value='0:00')
+    interval_pace = st.text_input('Interval_pace', value='0:00')
 
     # すべての入力ができているかチェック
-    if best_time == '00:00:00' or event == '' or freq == '' or easy_pace == '0:00' or threshold_pace == '0:00' or interval_pace == '0:00':
+    if best_time == '00:00:00' or event == '' or freq == '' or easy_pace == '0:00' or moderate_pace == '0:00' or threshold_pace == '0:00' or interval_pace == '0:00':
         st.warning('未入力の項目があります')
         submitted = False
     else:
@@ -49,22 +52,22 @@ def main():
         st.write(f'種目: {event}')
         st.write(f'自己ベスト: {best_time}')
         if freq == '4回/週':
-            distance = '週間走行距離50km'
+            distance = '50km'
         elif freq == '5回/週':
-            distance = '週間走行距離65km'
+            distance = '65km'
         elif freq == '6回/週':
-            distance = '週間走行距離85km'
+            distance = '85km'
         elif freq == '7回/週':
-            distance = '週間走行距離110km'
+            distance = '110km'
         else:
             distance = ''
         st.write(f'練習頻度: {freq}')
-        st.write(f'週間走行距離目安: {distance}')
+        st.write(f'週間走行距離目安: {distance}/週')
 
 
         # 追加: Easy Pace, Threshold Pace, Interval Pace の値を表示
-        pace_data = {'設定ペース': [easy_pace, threshold_pace, interval_pace]}
-        pace_df = pd.DataFrame(data=pace_data, index=['Easy Pace (/km)', 'Threshold Pace (/km)', 'Interval Pace (/km)'])
+        pace_data = {'設定ペース': [easy_pace, moderate_pace, threshold_pace, interval_pace]}
+        pace_df = pd.DataFrame(data=pace_data, index=['Easy Pace (/km)', 'Moderete Pace (/km)', 'Threshold Pace (/km)', 'Interval Pace (/km)'])
         st.table(pace_df.style.hide_index())
 
         week = ['月', '火', '水', '木', '金', '土', '日']
@@ -82,7 +85,7 @@ def main():
             elif i == 2:  # ペース走
                 menu = f'ペース走, 設定ペース{threshold_pace}/km, 20min'
             elif i == 5:  # ロングラン
-                menu = f'ロングラン, 設定ペース{easy_pace}/km, 90min'
+                menu = f'ロングラン, 設定ペース{moderate_pace}/km, 90min'
             else:
                 menu = f'Jog, 設定ペース{easy_pace}/km, 60min'
 
