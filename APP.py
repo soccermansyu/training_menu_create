3f1f8e5a1a7fe7d56df4cc639f9119d58b8b70a1
soccermansyu <118705210+soccermansyu@users.noreply.github.com>
2023-04-06 08:43:44 +0900

    Update APP.py

--- APP.py
+++ APP.py
@@ -133,8 +133,8 @@ def main():
 
         pace_data = {'設定ペース': [], '目標心拍数(回/分)': []}
         for pace, (min_val, max_val) in formatted_pace_ranges.items():
-            pace_data['設定ペース'].append(f'{min_val} - {max_val}/km')
-            pace_data['目標心拍数'].append(f'{hr_ranges[pace][0]} - {hr_ranges[pace][1]}回/分')
+            pace_data['設定ペース'].append(f'{min_val} - {max_val} /km')
+            pace_data['目標心拍数'].append(f'{hr_ranges[pace][0]} - {hr_ranges[pace][1]} 回/分')
 
         pace_df = pd.DataFrame(data=pace_data, index=['Easy Pace', 'Moderate Pace', 'Threshold Pace', 'CV Pace', 'Interval Pace', 'Repetition Pace'])
         st.table(pace_df.style.hide_index())
