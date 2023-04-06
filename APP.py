a3163a0a3d389cfbbdcb747ddcb169384d91bf7c
soccermansyu <118705210+soccermansyu@users.noreply.github.com>
2023-04-07 04:41:57 +0900

    Update APP.py

--- APP.py
+++ APP.py
@@ -18,12 +18,14 @@ def main():
     else:
         distance = 42195
 
-    st.write('2. 自己ベスト (hh:mm:ss)')
-    best_time = st.text_input('best_time', value='00:00:00')
+    #st.write('2. 自己ベスト (hh:mm:ss)')
+    #best_time = st.text_input('best_time', value='00:00:00')
     
-    #st.write('2. 自己ベスト')
-    #best_time = st.time_input('best_time', value=datetime.time(hour=0, minute=0, second=0), scrollwheel=True)
-
+    st.write('2. 自己ベスト (hh:mm:ss)')
+    best_time = st.slider('best_time', value=datetime.time(hour=0, minute=0, second=0),
+                          format='HH:mm:ss', step=datetime.timedelta(seconds=1),
+                          max_value=datetime.time(hour=23, minute=59, second=59))
+    best_time = best_time.strftime('%H:%M:%S')
     
     st.write('3. 目標とする種目')
     event2 = st.selectbox('event2', ['5000m', '10000m', 'ハーフマラソン', 'フルマラソン'])
