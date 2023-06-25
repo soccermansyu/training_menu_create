48f6790b8dd92fab7003dcbd519b1bf76d840d0a
soccermansyu <118705210+soccermansyu@users.noreply.github.com>
2023-06-26 05:56:54 +0900

    Update APP.py

--- APP.py
+++ APP.py
@@ -197,9 +197,7 @@ def main():
                     menu = f'ロングラン, 設定ペース{moderate_pace_min}~{moderate_pace_max}/km, 90min'
                 else:
                     menu = f'Jog, 設定ペース{easy_pace_min}~{easy_pace_max}/km, 60min'
-                # df = df.append({'曜日': week[i], 'トレーニングメニュー': menu}, ignore_index=True)
-                new_row = [week[i], menu]
-                df = df.append(pd.Series(new_row, index=df.columns), ignore_index=True)
+                df = df.append({'曜日': week[i], 'トレーニングメニュー': menu}, ignore_index=True)
 
         elif freq in ['4 回/週', '5 回/週']:
             for i in range(7):
@@ -225,9 +223,7 @@ def main():
                     menu = f'ロングラン, 設定ペース{moderate_pace_min}~{moderate_pace_max}/km, 90min'
                 else:
                     menu = f'Jog, 設定ペース{easy_pace_min}~{easy_pace_max}/km, 60min'
-                 # df = df.append({'曜日': week[i], 'トレーニングメニュー': menu}, ignore_index=True)
-                new_row = [week[i], menu]
-                df = df.append(pd.Series(new_row, index=df.columns), ignore_index=True)
+                df = df.append({'曜日': week[i], 'トレーニングメニュー': menu}, ignore_index=True)
 
         else:
             for i in range(7):
@@ -254,10 +250,7 @@ def main():
                     menu = f'ロングラン, 設定ペース{moderate_pace_min}~{moderate_pace_max}/km, 120min'
                 else:
                     menu = f'Jog, 設定ペース{easy_pace_min}~{easy_pace_max}/km, 60min'
-                # df = df.append({'曜日': week[i], 'トレーニングメニュー': menu}, ignore_index=True)
-                new_row = [week[i], menu]
-                df = df.append(pd.Series(new_row, index=df.columns), ignore_index=True)
-
+                df = df.append({'曜日': week[i], 'トレーニングメニュー': menu}, ignore_index=True)
                 
         # 表形式でトレーニングメニューを出力
         st.table(df.set_index('曜日'))
